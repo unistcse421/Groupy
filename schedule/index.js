ab813dbf58ad6586faf0c0f70b959ef565dd1106
xogus <kgyoo8232@gmail.com>
2016-05-09 17:16:10 +0900

    modify schedule setting

--- schedule/index.js
+++ schedule/index.js
@@ -2,7 +2,5 @@
  * Created by Taehyun on 2016-05-09.
  */
 
-var cronTimes = require('./cronTimes');
-
-exports.getGroupMessageAndUpdate        = require('./getGroupMessageAndUpdate')(cronTimes[0]);
-exports.checkGroupMessagesAreDeleted    = require('./checkGroupMessagesAreDeleted')(cronTimes[1]);
+exports.getGroupMessageAndUpdate        = require('./getGroupMessageAndUpdate')('0,30 * * * * *');
+exports.checkGroupMessagesAreDeleted    = require('./checkGroupMessagesAreDeleted')('15 */5 * * * *');
