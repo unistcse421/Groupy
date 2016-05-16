60ed0323ad8afde6589c6a06fdb1a6f971cf83ce
kimxogus <kgyoo8232@gmail.com>
2016-05-16 20:43:01 +0900

    add facebook service

--- public/src/js/service/FacebookService.js
+++ public/src/js/service/FacebookService.js
@@ -7,5 +7,43 @@ define(['app'], function(app) {
     app.service('FacebookService', ['$http', '$q',
         function($http, $q) {
             var _this = this;
+            _this.loginStatus = null;
+            
+            _this.getLoginStatus = function() {
+                var deferred = $q.defer();
+                
+                FB.getLoginStatus(function(res) {
+                    if(res.status === 'connected') {
+                        _this.loginStatus = res.status;
+                        deferred.resolve(res.status);
+                    } else {
+                        _this.loginStatus = res.status;
+                        deferred.reject(res.status);
+                    }
+                });
+                
+                return deferred.promise;
+            };
+            
+            _this.login = function() {
+                var deferred = $q.defer();
+                
+                FB.login(function(res) {
+                    if(res.status === 'connected') {
+                        _this.loginStatus = res.status;
+                        deferred.resolve(res.status);
+                    } else {
+                        _this.loginStatus = res.status;
+                        deferred.reject(res.status);
+                    }
+                });
+                
+                return deferred.promise;
+            };
+            
+            _this.getPostInfo = function(id) {
+                var deferred = $q.defer();
+                
+            }
         }]);
 });
