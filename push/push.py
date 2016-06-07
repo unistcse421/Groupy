c0736d42aeddbe7d8aedb01af66f87422f906cda
L34p <hmg0228@gmail.com>
2016-06-07 18:53:04 +0900

    push server

--- push/push.py
+++ push/push.py
@@ -9,12 +9,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # DB configurations
-host = ""
-db_id = ""
-db_pw = ""
-db_name = ""
 
-apns = APNs(use_sandbox=True, cert_file='cert.pem', key_file='key.pem')
+apns = APNs(use_sandbox=True, cert_file='/Users/leap/UNIST/DB/keys/cert.pem', key_file='/Users/leap/UNIST/DB/keys/key.pem')
 db = MySQLdb.connect(host, db_id, db_pw, db_name)
 cursor = db.cursor()
 
@@ -32,45 +28,6 @@ def is_exist(mid, gid):
     else:
         return False
 
-def update_msg(message, updated_time, message_id):
-    cursor.execute ("""
-        UPDATE message
-        SET message=%s, updated_time=%s
-        WHERE id=%s
-    """, (message, updated_time, message_id))
-
-def insert_msg(message_id, group_id, message, created_time, updated_time, tags):
-    cursor.execute("""
-        INSERT INTO message
-        (id, group_id, message, created_time, updated_time)
-        VALUES (%s, %s, %s, %s, %s)
-    """, (message_id, group_id, message, created_time, updated_time))
-
-    tag_list = list()
-    for t in tags:
-        tag_list.append(t['hashtag'].encode('utf8mb4'))
-
-    if tag_list:
-        q_base = "INSERT IGNORE INTO message_hashtag (message_id, hashtag) VALUES "
-        temp_list = list()
-        for t in tag_list:
-            temp_list.append("(\'{}\')".format(t))
-
-        q_base += ", ".join(temp_list)
-        cursor.execute(q_base)
-
-        temp_list = list()
-        for t in tag_list:
-            temp_list.append((message_id, t))
-
-        cursor.executemany("""
-            INSERT IGNORE INTO message_hashtag
-            (message_id, hashtag) VALUES (%s, %s)
-        """, temp_list)
-
-def commit():
-    db.commit()
-
 for line in sys.stdin:
     j = json.loads(line)
 
@@ -80,18 +37,8 @@ for line in sys.stdin:
     created_time = time_formater(j['created_time'])
     updated_time = time_formater(j['updated_time'])
 
-'''
-    # Message is already exist
-    if is_exist(message_id, group_id):
-        update_msg(message, updated_time, message_id)
-        commit();
-'''
-
     # New message
     if not is_exist(message_id, group_id):
-        #insert_msg(message_id, group_id, message, created_time, updated_time, j['hashtags'])
-        #commit();
-
         cursor.execute("""
             SELECT uuid, push_keyword FROM push_info
             WHERE group_id=%s AND uuid in
